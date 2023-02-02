54213e213ce19c2b75ca4595c6ead70da7c78c7f
HyeokJin96 <gurwls7590@gmail.com>
2023-02-02 19:40:31 +0900

    PlayerControll and Background modify

--- CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/03.PlayScene/PlayerCotroller.cs
+++ CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/03.PlayScene/PlayerCotroller.cs
@@ -10,15 +10,28 @@ public class PlayerCotroller : MonoBehaviour
     public float speed = default;
     public float jumpForce = default;
 
+    private bool isGrounded = false;
+    private bool isDead = false;
+
+    private Animator playerAni = default;
+    private AudioSource playerAudio = default;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAni = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead == true)
+        {
+            return;
+        }   //  if : 사망시 종료
+
+        //  { 플레이어 이동 관련 로직 
         float xInput = Input.GetAxis("Horizontal");
 
         if (xInput > 0f)
@@ -42,10 +55,34 @@ public class PlayerCotroller : MonoBehaviour
             }
         }
 
+        playerAni.SetBool("Grounded", isGrounded);
+        //  } 플레이어 이동 관련 로직
     }
 
+    //! 사망처리
     public void Die()
     {
-        gameObject.SetActive(false);
+        playerAni.SetTrigger("Die");
+
+        playerAudio.Play();
+
+        playerRigidbody.velocity = Vector2.zero;
+        isDead = true;
     }
+
+    //! 충돌체 감지
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (0.7f < collision.contacts[0].normal.y)
+        {
+            isGrounded = true;
+        }   //  if : 45도 보다 완만한 땅을 밟은 경우
+    }
+
+    //! 충돌체 감지
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2023-02-01 / v0.0.1 / Set UI
 2023-02-01 / v0.0.2 / PlayerControll
 2023-02-02 / v0.0.3 / CameraControll
+2023-02-02 / v0.0.4 / PlayerControll and Background modify
