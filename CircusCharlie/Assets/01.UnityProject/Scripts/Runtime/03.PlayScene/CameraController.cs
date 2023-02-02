351727da46244e755d320f48e20340ac439ddacd
HyeokJin96 <gurwls7590@gmail.com>
2023-02-02 18:02:21 +0900

    CameraControll

--- CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/03.PlayScene/CameraController.cs
+++ CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/03.PlayScene/CameraController.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float speed = default;
-    public GameObject player = default;
+    public Transform target = default;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.transform.position - this.transform.position;
-        Vector3 move = new Vector3(direction.x * speed * Time.deltaTime, 0f, 0f);
-        this.transform.Translate(move);
+        transform.position = new Vector3(target.position.x, 0f, transform.position.z);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CircusCharlie
 
 2023-02-01 / v0.0.1 / Set UI
+2023-02-01 / v0.0.2 / PlayerControll
+2023-02-02 / v0.0.3 / CameraControll
